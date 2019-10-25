5e88fd605bfad2cd388887ed6864bc4d83d4c785
Fernando Moreira <fernandoepm.pro@gmail.com>
2019-10-24 22:50:29 -0300

    Adicionando classes Resultado e Pergunta

---
+++ showdomilhao.h
@@ -0,0 +1,53 @@
+#ifndef SHOWDOMILHAO_H
+#include SHOWDOMILHAO_H
+
+using namespace std;
+
+class Resultado{
+
+private:
+    string nomePessoa;
+    float pontuacao;
+    int dia, mes, ano;
+
+public:
+    Resultado();
+
+    string get_nomePessoa();
+    float get_pontuacao();
+    int get_dia();
+    int get_mes();
+    int get_ano();
+
+    void set_nomePessoa(string nomePessoa);
+    void set_pontuacao(float pontuacao);
+    void set_dia(int dia);
+    void set_mes(int mes);
+    void set_ano(int ano);
+
+};
+
+class Pergunta{
+
+private:
+    int respostaCorreta;
+    int dificuldade;
+    string pergunta;
+
+public:
+    Pergunta();
+
+    int get_respostaCorreta();
+    int get_dificuldade();
+    string get_pergunta();
+
+    void set_respostaCorreta(int respostaCorreta);
+    void set_dificuldade(int dificuldade);
+    void set_pergunta(string pergunta);
+
+    bool verificaAcerto(int respostaCorreta);
+    void mostraAlternativas();
+
+};
+
+#endif // SHOWDOMILHAO_H
